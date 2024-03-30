0903f089a5f7d9300bd7ef48cdb46295fec4d00c
Tangent Chang <andy23512@gmail.com>
2024-03-30 18:30:17 +0800

    feat: make individual CC1 and M4G layouts and keep original standard and hsu layouts

--- Source/Preferences.swift
+++ Source/Preferences.swift
@@ -159,6 +159,8 @@ struct CandidateListTextSize {
     case eten26 = 3
     case hanyuPinyin = 4
     case IBM = 5
+    case CC1 = 6
+    case M4G = 7
 
     var name: String {
         return switch self {
@@ -174,6 +176,10 @@ struct CandidateListTextSize {
             "HanyuPinyin"
         case .IBM:
             "IBM"
+        case .CC1:
+            "CC1"
+        case .M4G:
+            "M4G"
         }
     }
 }
